d1b2f037c8339f375925146399602aecd82dcb12
migueltorralbo <miguel.torralbo@travelperk.com>
2022-04-13 13:35:48 +0200

    Added tags

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+def sample_user(email='test@travelperk.com', password='test123'):
+    return get_user_model().objects.create_user(email, password)
+
 
 
 class ModelTests(TestCase):
@@ -33,6 +38,13 @@ class ModelTests(TestCase):
             'test@travelperk.com',
             'test123'
         )
-
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
